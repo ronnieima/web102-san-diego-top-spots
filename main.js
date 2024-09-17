dd87b5f7e1963f4c08b9403c14e9c106fdee5769
Ronnie Kaito Imagawa <ronnie.imagawa@cccareers.org>
2024-09-17 13:18:31 -0400

    populate table with json data

--- main.js
+++ main.js
@@ -1,7 +1,28 @@
+const FIELDS = ["name", "description", "location"];
+
 $(document).ready(function () {
-  $.getJSON("demo_ajax_json.js", function (result) {
-    $.each(result, function (i, field) {
-      $("div").append(field + " ");
+  $.getJSON("data.json", function (spots) {
+    spots.map((spot) => {
+      const newRow = document.createElement("tr");
+
+      for (const field of FIELDS) {
+        if (field === "location") {
+          const [lat, lng] = spot[field]; // Destructuring the "location" array
+          const mapLink = document.createElement("a");
+          mapLink.innerText = "Open in Google Maps";
+          mapLink.target = "_blank"; // this opens a new tab on click
+          mapLink.href = `https://www.google.com/maps?q=${lat},${lng}`;
+
+          newRow.appendChild(mapLink);
+        } else {
+          const newCell = document.createElement("td");
+          newCell.innerHTML = spot[field];
+          newRow.appendChild(newCell);
+        }
+      }
+
+      const table = document.getElementById("spots-table");
+      table.appendChild(newRow);
     });
   });
 });
